bae6bd48a3def3e8c249f34d00923f3fa0c29602
RAJUYEOB <pag1yun99@gmail.com>
2024-10-10 17:52:24 +0900

    MyPageCss

--- src/components/menus/MobileHeader.jsx
+++ src/components/menus/MobileHeader.jsx
@@ -6,7 +6,7 @@ const MobileHeader = ({ children, className }) => {
   const navigate = useNavigate();
   return (
     <div
-      className={`${className} font-extrabold w-full h-12 xl:h-14 z-10 border-b-2 bg-white border-b-slate-200 flex items-center`}
+      className={`${className} flex font-extrabold w-full h-12 xl:h-14 z-10 border-b-2 bg-white border-b-slate-200 items-center`}
     >
       <button onClick={() => navigate(-1)} className="p-3">
         <img src={back} alt="back" />
